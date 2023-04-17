756008ac33a7670607ee7fa4b15521fcd6f5c782
HenryZ5734 <200110614@stu.hit.edu.cn>
2023-04-17 13:19:48 +0800

    完成Lab2 以太网协议

--- src/ethernet.c
+++ src/ethernet.c
@@ -11,6 +11,17 @@
 void ethernet_in(buf_t *buf)
 {
     // TO-DO
+    // 1. 检查数据包长度是否合法
+    if(buf->len < sizeof(ether_hdr_t)){
+        return;
+    }
+
+    // 2. 移除以太网包头
+    uint8_t *origin_data = buf->data;
+    buf_remove_header(buf, sizeof(ether_hdr_t));
+
+    // 3. 向上层传递数据包
+    net_in(buf, swap16(((ether_hdr_t *)origin_data)->protocol16), ((ether_hdr_t *)origin_data)->src);
 }
 /**
  * @brief 处理一个要发送的数据包
@@ -22,6 +33,23 @@ void ethernet_in(buf_t *buf)
 void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol)
 {
     // TO-DO
+    // 1. 将数据长度补充到46字节
+    if(buf->len < ETHERNET_MIN_TRANSPORT_UNIT){
+        buf_add_padding(buf, ETHERNET_MIN_TRANSPORT_UNIT - buf->len);
+    }
+
+    // 2. 添加以太网头
+    buf_add_header(buf, sizeof(ether_hdr_t));
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+
+    // 3. 填充以太网头
+    memcpy(hdr->dst, mac, NET_MAC_LEN);
+    memcpy(hdr->src, net_if_mac, NET_MAC_LEN);
+    hdr->protocol16 = swap16(protocol);
+
+    // 4. 发送数据包
+    driver_send(buf);
+
 }
 /**
  * @brief 初始化以太网协议
